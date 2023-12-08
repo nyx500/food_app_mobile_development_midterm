6637c871db82afce8893acaa63fcb855e9a7c345
Ophelia <ophelia.lavey2@gmail.com>
2023-12-08 11:49:20 +0100

    First commit

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// Import Navigation libraries
+import {NavigationContainer} from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+// Import TableView components
+import { Cell, Section, TableView } from 'react-native-tableview-simple';
+
 
 export default function App() {
   return (
