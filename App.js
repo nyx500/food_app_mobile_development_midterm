f0c7fee17664a74feaefece96af42b2a56b21a69
Ophelia <ophelia.lavey2@gmail.com>
2023-12-09 14:18:21 +0100

    Made some extra restaurants

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 // Image Attributions: 
-// Unsplash.com
-// ICe cream parlor photo by
+// Unsplash.com photos
+// Ice cream parlor photo by
 // <a href="https://unsplash.com/@walexy?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Bill Alexy</a> on <a href="https://unsplash.com/photos/an-ice-cream-parlor-with-a-wooden-bench-in-front-of-it-1vDN-XW6yow?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
-  
+// Vegan restaurant photo:
+// Photo by <a href="https://unsplash.com/@courtneymcook?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Courtney Cook</a> on <a href="https://unsplash.com/photos/flat-lay-photography-pf-food-on-table-SgHyqMzLOMA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+// Indian curry photo:
+// Photo by <a href="https://unsplash.com/@grimnoire?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">emy</a> on <a href="https://unsplash.com/photos/flat-lay-photography-of-vegetable-soup-on-white-ceramic-cup-XoByiBymX20?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
 
 import { StatusBar } from 'expo-status-bar';
 import { Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 // Import Navigation libraries
-import {NavigationContainer} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 // Import TableView components
 import { Cell, Section, TableView } from 'react-native-tableview-simple';
@@ -15,9 +18,9 @@ import { Cell, Section, TableView } from 'react-native-tableview-simple';
 // Screens go here
 
 // Restaurants/Home Screen
-function HomeScreen({navigation}) {
+function HomeScreen({ navigation }) {
 
-  
+
   const HomeScreenCell = (props) => (
     // In your HomescreenCell, use props to set the height to (FIXED) 290px, background colour to transparent and highlight colour to #ccc.
     <Cell
@@ -32,7 +35,7 @@ function HomeScreen({navigation}) {
           <Image
             style={styles.restaurantImage}
             source={props.imgUri}
-            resizeMode="cover"/>
+            resizeMode="cover" />
           <View style={styles.etaView}>
             <Text style={styles.etaText}>
               {props.eta}{'\n'}mins
@@ -44,7 +47,7 @@ function HomeScreen({navigation}) {
           <Text style={styles.restaurantTaglineText}>
             {props.tagline}
           </Text>
-        </View>   
+        </View>
       }
       onPress={props.action}
     />
@@ -55,78 +58,214 @@ function HomeScreen({navigation}) {
     <View>
       <ScrollView>
         <TableView>
-          <Section 
-            header={""} 
+          <Section
+            header={""}
             separatorTintColor="#ccc"
             hideSeparator={true}
           >
             <HomeScreenCell
-              title = {"Joe's Gelato"}
-              tagline = {"Desert, Ice cream, £££"}
-              eta = "10-30"
-              imgUri = {require("./images/ice_cream_parlor.jpg")}
-              action = {() => {navigation.navigate("Menu", 
-              // Pass "items" object in as a route parameter
-              { 
-                // "items" contains an array containing Sections for categories of food and drink
-                items: [
-                  // Each object in the array stores a food-subcategory
+              title={"Gaia's Garden"}
+              tagline={"Vegan and vegetarian favourites, ££"}
+              eta="20-25"
+              imgUri={require("./images/vegan_restaurant.jpg")}
+              action={() => {
+                navigation.navigate("Menu",
+                  // Pass "items" object in as a route parameter
                   {
-                    "title":"Gelato",
-                    /** Each food-subcategory contains a "contents" property storing an array
-                     * of details about separate food items such as "title" */
-                    "contents":[
-                      {"title": "Chocolate"},
-                      {"title": "Dulce de leche"},
-                      {"title": "Hazelnut"},
-                      {"title": "Lemon"},
-                      {"title": "Mint"},
-                      {"title": "Sicilian orange"},
-                      {"title": "Strawberry"},
-                      {"title":"Vanilla"},
-                      {"title": "Yogurt and cherries"}
+                    // "items" contains an array containing Sections for categories of food and drink
+                    items: [
+                      // Each object in the array stores a food-subcategory
+                      {
+                        "title": "Salads",
+                        /** Each food-subcategory contains a "contents" property storing an array
+                         * of details about separate food items such as "title" */
+                        "contents": [
+                          { "title": "Caesar Salad: veggie chicken fillets and cashew cheese" },
+                          { "title": "California Salad: avocado, tomatoes, vegan bacon and corn-on-the-cob" },
+                          { "title": "Winter Salad: sweet potatoes, brussel sprouts and walnuts" },
+                          { "title": "Summer Refresher: blueberry, vegan feta and baby spinach" },
+                          { "title": "Thai Salad: mango, tofu and peanut salad" }
+                        ]
+                      },
+                      {
+                        "title": "Vegan Burgers",
+                        "contents": [
+                          { "title": "California Dreaming: vegan beef patty with spicy guacamole" },
+                          { "title": "Marine Dream: vegan fish patty with gherkins and seaweed mayo" },
+                          { "title": "Smokehouse Supreme: vegan beef patty with barbecue sauce and onion rings" },
+                          { "title": "The CSB: faux chicken with spicy red cabbage slaw" },
+                          { "title": "The OG: vegan beef patty with bacon and vegan cheese" }
+                        ]
+                      },
+                      {
+                        "title": "Sides",
+                        "contents": [
+                          { "title": "Big load fries" },
+                          { "title": "Crispy vegan chicken nuggets" },
+                          { "title": "Onion rings" },
+                          { "title": "Sweet potato wedges" },
+                          { "title": "Waffle fries" },
+                          { "title": "Vegan slaw" },
+                        ]
+                      },
+                      {
+                        "title": "Sweets",
+                        "contents": [
+                          { "title": "Banana muffin" },
+                          { "title": "Carrot cake" },
+                          { "title": "Coconut and passion fruit sticky rice" },
+                          { "title": "Mango chia bowl" },
+                          { "title": "Peach pie" },
+                          { "title": "Vegan chocolate fondant with sticky fudge" }
+                        ]
+                      },
+                      {
+                        "title": "Drinks",
+                        "contents": [
+                          { "title": "Berry smoothie" },
+                          { "title": "Fresh-squeezed orange juice" },
+                          { "title": "Kale and spinach smoothie" },
+                          { "title": "Mango smoothie" },
+                          { "title": "Sparkling water" },
+                        ]
+                      }
                     ]
-                  },
+                  });
+              }}
+            />
+            <HomeScreenCell
+              title={"Joe's Gelato"}
+              tagline={"Desert, Ice cream, £££"}
+              eta="10-30"
+              imgUri={require("./images/ice_cream_parlor.jpg")}
+              action={() => {
+                navigation.navigate("Menu",
+                  // Pass "items" object in as a route parameter
                   {
-                    "title": "Special Desserts",
-                    "contents": [
-                      {"title": "Apple pie"},
-                      {"title": "Carrot cake"},
-                      {"title": "Coconut pie"},
-                      {"title": "Crêpes with 2 gelato scoops"},
-                      {"title": "Hazelnut and chocolate torte"},
-                      {"title": "Raspberry cheesecake"},
-                      {"title": "Waffles with 2 gelato scoops"}
+                    // "items" contains an array containing Sections for categories of food and drink
+                    items: [
+                      // Each object in the array stores a food-subcategory
+                      {
+                        "title": "Gelato",
+                        /** Each food-subcategory contains a "contents" property storing an array
+                         * of details about separate food items such as "title" */
+                        "contents": [
+                          { "title": "Chocolate" },
+                          { "title": "Dulce de leche" },
+                          { "title": "Hazelnut" },
+                          { "title": "Lemon" },
+                          { "title": "Mint" },
+                          { "title": "Sicilian orange" },
+                          { "title": "Strawberry" },
+                          { "title": "Vanilla" },
+                          { "title": "Yogurt and cherries" }
+                        ]
+                      },
+                      {
+                        "title": "Special Desserts",
+                        "contents": [
+                          { "title": "Apple pie" },
+                          { "title": "Carrot cake" },
+                          { "title": "Coconut pie" },
+                          { "title": "Crêpes with 2 gelato scoops" },
+                          { "title": "Hazelnut and chocolate torte" },
+                          { "title": "Raspberry cheesecake" },
+                          { "title": "Waffles with 2 gelato scoops" }
+                        ]
+                      },
+                      {
+                        "title": "Coffee",
+                        "contents": [
+                          { "title": "Caffé Americano" },
+                          { "title": "Cappucino" },
+                          { "title": "Caramel Macchiato" },
+                          { "title": "Filter Coffee" },
+                          { "title": "Flat white" },
+                          { "title": "Latte" },
+                          { "title": "Mocha" },
+                        ]
+                      },
+                      {
+                        "title": "Milkshakes",
+                        "contents": [
+                          { "title": "Chocolate" },
+                          { "title": "Dulce de leche" },
+                          { "title": "Hazelnut" },
+                          { "title": "Lemon" },
+                          { "title": "Mint" },
+                          { "title": "Sicilian orange" },
+                          { "title": "Strawberry" },
+                          { "title": "Vanilla" },
+                          { "title": "Yogurt and cherries" }
+                        ]
+                      }
                     ]
-                  },
+                  });
+              }}
+            />
+            <HomeScreenCell
+              title={"Spice Paradise"}
+              tagline={"Curry in a hurry"}
+              eta="15-25"
+              imgUri={require("./images/curry.jpg")}
+              action={() => {
+                navigation.navigate("Menu",
                   {
-                    "title": "Coffee",
-                    "contents": [
-                      {"title": "Caffé Americano"},
-                      {"title": "Cappucino"},
-                      {"title": "Caramel Macchiato"},
-                      {"title": "Filter Coffee"},
-                      {"title":"Flat white"},
-                      {"title": "Latte"},
-                      {"title": "Mocha"},
+                    items: [
+                      {
+                        "title": "Starters",
+                        "contents": [
+                          { "title": "Chicken samosas" },
+                          { "title": "Lentil bites" },
+                          { "title": "Onion bhajis" },
+                          { "title": "Potato paratha" },
+                          { "title": "Vegetarian pakoras" },
+                          { "title": "Vegetarian samosas" }
+                        ]
+                      },
+                      {
+                        "title": "Curries",
+                        "contents": [
+                          { "title": "Chicken korma" },
+                          { "title": "Chicken tikka masala" },
+                          { "title": "Chicken jalfrezi" },
+                          { "title": "Chicken vindaloo" },
+                          { "title": "Chickpea chana masala" },
+                          { "title": "Fish coconut curry" },
+                          { "title": "Lamb biryani" },
+                          { "title": "Lamb madras" },
+                          { "title": "Lentil dahl" },
+                          { "title": "Prawn jalfrezi" },
+                          { "title": "Vegetarian balti" },
+                          { "title": "Vegetarian biryani" }
+                        ]
+                      },
+                      {
+                        "title": "Sides",
+                        "contents": [
+                          { "title": "Coconut rice" },
+                          { "title": "Pilau rice" },
+                          { "title": "Plain rice" },
+                          { "title": "Garlic naan bread" },
+                          { "title": "Plain naan bread" },
+                          { "title": "Cucumber raita" },
+                          { "title": "Selection of pickles" },
+                          { "title": "Poppadums" },
+                        ]
+                      },
+                      {
+                        "title": "Drinks",
+                        "contents": [
+                          { "title": "Chai masala" },
+                          { "title": "Home-made lemonade" },
+                          { "title": "Salty lassi" },
+                          { "title": "Mango lassi" },
+                          { "title": "Sparkling water" }
+                        ]
+                      }
                     ]
-                  },
-                  {
-                    "title": "Milkshakes",
-                    "contents": [
-                      {"title": "Chocolate"},
-                      {"title": "Dulce de leche"},
-                      {"title": "Hazelnut"},
-                      {"title": "Lemon"},
-                      {"title": "Mint"},
-                      {"title": "Sicilian orange"},
-                      {"title": "Strawberry"},
-                      {"title":"Vanilla"},
-                      {"title": "Yogurt and cherries"}
-                    ]
-                  }
-                ]
-              });}}
+                  });
+              }}
             />
           </Section>
         </TableView>
@@ -136,27 +275,27 @@ function HomeScreen({navigation}) {
 }
 
 // Menu Screen: remember to pass in the route containing the "items" data
-function MenuScreen({route, navigation}) {
+function MenuScreen({ route, navigation }) {
   return (
-    <View>  
+    <View>
       <ScrollView>
         <TableView>
           {
             // Inside the TableView iterate through "items", creating a new Section each time.
-            route.params.items.map((item, idx ) => (
-              <Section 
-                key = {idx}
+            route.params.items.map((item, idx) => (
+              <Section
+                key={idx}
                 header={item.title}
               >
-               {
-                item.contents.map((product, idx2) => (
-                  <Cell
-                    key={idx2}
-                    cellStyle="Basic"
-                    title = {product.title}
-                  />
-                )) 
-               }
+                {
+                  item.contents.map((product, idx2) => (
+                    <Cell
+                      key={idx2}
+                      cellStyle="Basic"
+                      title={product.title}
+                    />
+                  ))
+                }
               </Section>
             ))
           }
@@ -171,12 +310,12 @@ const Stack = createStackNavigator();
 export default function App() {
 
   return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Restaurants" component={HomeScreen}/>
-          <Stack.Screen name="Menu" component={MenuScreen}/>
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Restaurants" component={HomeScreen} />
+        <Stack.Screen name="Menu" component={MenuScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -190,15 +329,15 @@ const styles = StyleSheet.create({
   restaurantCellContentContainer: {
     // Set height of cell to 290px
     height: 290,
-    alignItems:"stretch"
+    alignItems: "stretch"
   },
   restaurantCellContentView: {
     // Expands the View in CellContent horizontally to fill whole width of screen
-    flex:1,
+    flex: 1,
     justifyContent: "center",
-    alignItems:"flex-start", // Text goes to left (except for absolutely-positioned ETA View)
+    alignItems: "flex-start", // Text goes to left (except for absolutely-positioned ETA View)
     paddingVertical: "5%" // Leaves some responsive padding around the <View> in the Cell
-  },  
+  },
   restaurantImage: {
     width: "100%",
     height: "74%",
@@ -230,5 +369,5 @@ const styles = StyleSheet.create({
   },
   restaurantTaglineText: {
     color: "#858585" // grey tagline
-  }  
+  }
 });
